9b2c97df3ca55f2899f6faf8228f4906c87e0b25
n8shade <nebir01750@gmail.com>
2025-03-02 01:46:53 +0600

    buttons disabled

--- cards.js
+++ cards.js
@@ -2,5 +2,6 @@ const buttons = document.querySelectorAll(".completed");
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         alert("Button clicked!");
+        event.target.disabled = true;
     });
 });
